f44d3b786236eb4bd3e6f02f0c5426356a97e882
3mocki <starleesm@gmail.com>
2019-03-14 22:54:44 -0700

    031419

--- Collector.py
+++ Collector.py
@@ -106,16 +106,16 @@ def write_rad(numberOfData, csvRowCount):
 
 
 def init_row():
-    f = open('O3_RAW.csv', 'a', newline='')
+    f = open('CO_RAW.csv', 'a', newline='')
     wr = csv.writer(f)
     wr.writerow(row)
     f.close()
 
 
 def write_raw():
-    f = open('O3_RAW.csv', 'a', newline='')
+    f = open('CO_RAW.csv', 'a', newline='')
     wr = csv.writer(f)
-    wr.writerow(o3_Raw_data)
+    wr.writerow(co_Raw_data)
     f.close()
 
 
