150b8ef4499abf12d048be584244f0158433af58
3mocki <starleesm@gmail.com>
2019-03-20 21:17:10 -0700

    032019

--- Switch.py
+++ Switch.py
@@ -7,7 +7,10 @@ def init_Switch():
 
 
 if __name__== '__main__':
+    print("This is switch for operating sensor.")
+    print("Please push the button! :)")
     init_Switch()
     while True:
         if GPIO.input(6) == 1:
+            print("===== Operating Sensor =====")
             os.system("python3 Collector.py")
